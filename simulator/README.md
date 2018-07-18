c788dba05e8fc3adeb1d7950de372b8478f416b0
rodrigo <rodrigoperez93@gmail.com>
2018-07-18 16:11:54 -0300

    First commit, simulator.py works OK

---
+++ simulator/README.md
@@ -0,0 +1,8 @@
+# Notes about sampling
+
+The simulator.py script obtaines samples of a system given a 1D free energy profile. Parameters should me modified within the script. Run with the following command, 
+
+```
+python simulator.py -s samples.dat -sp sampledprob.dat -p exactprob.dat -g freeenergy.dat
+```
+
---
+++ simulator/simulator.py
@@ -0,0 +1,55 @@
+# Example run command
+# python simulator.py -s samples.dat -sp sampledprob.dat -p exactprob.dat -g freeenergy.dat
+
+import numpy as np
+import argparse
+
+
+# --------------------------------------
+#     Parse                
+# --------------------------------------
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-s" , dest="samples"           , required=True)
+parser.add_argument("-sp", dest="sampledProbability", required=True)
+parser.add_argument("-p" , dest="exactProbability"  , required=True)
+parser.add_argument("-g" , dest="freeEnergy"        , required=True)
+
+args = parser.parse_args()
+samplesFile            = open(args.samples,'w')
+sampledProbabilityFile = open(args.sampledProbability,'w')
+exactProbabilityFile   = open(args.exactProbability,'w')
+freeEnergyFile         = open(args.freeEnergy,'w')
+
+#--------------------------------------------------
+T        = 310.0        #in K
+Kb       = 0.0019872041 #in kcal/mol/K
+beta     = 1/(Kb*T)       #in kcal/mol
+Nsamples = 1000000
+
+dX = 1e-4
+X  = np.arange(-1.0,1.0+1e-10,dX)
+G  = 4*X**2 
+P  = np.exp(-beta*G)
+P /= np.sum(P)
+
+s = np.random.choice(X,size=Nsamples,p=P)
+
+h,e = np.histogram(s,bins=100,density=True)
+e  = (e[0:-1]+e[1:])/2
+
+for i in range(G.size):
+        freeEnergyFile.write("%14.6g %14.6g\n" %(X[i],G[i]))
+freeEnergyFile.close()
+
+for i in range(P.size):
+        exactProbabilityFile.write("%14.6g %14.6g\n" %(X[i],P[i]/dX))
+exactProbabilityFile.close()
+
+for i in range(s.size):
+	samplesFile.write("%14.6g\n" %s[i])
+samplesFile.close()
+
+for i in range(h.size):
+        sampledProbabilityFile.write("%14.6g %14.6g\n" %(e[i],h[i]))
+sampledProbabilityFile.close()
